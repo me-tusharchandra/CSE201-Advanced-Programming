f2a1aa1b67ab56226e91e0e9bd1be2df29b3e151
Tushar Chandra <64960756+me-tusharchandra@users.noreply.github.com>
2022-09-17 00:53:18 +0530

    Code base
    writing the base code for the application

---
+++ Assignment_1/src/Main.java
@@ -0,0 +1,73 @@
+//Assignment 1
+//Made by Tushar Chandra-2021211
+
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+//        int app0 = 0, app1 = 0;
+//        System.out.println("Welcome to FutureBuilder");
+//        System.out.println("    1.) Enter the Application");
+//        System.out.println("    2.) Exit the Application");
+//        Scanner sc = new Scanner(System.in); //input scanner
+//
+//        while(app0 != 2){ //application loop
+//            app0 = sc.nextInt();
+//            if(app0>2){  //ErrorGen: Warning if input is anything other than 1,2
+//                System.out.println("Wrong choice entered. Exiting...");
+//                break;
+//            }
+//            else{
+//                System.out.println("Choose the mode you want to Enter in:- ");
+//                System.out.println("    1.) Enter as Student Mode");
+//                System.out.println("    2.) Enter as Company Mode");
+//                System.out.println("    3.) Enter as Placement Cell Mode");
+//                System.out.println("    4.) Return to main Application");
+//                app1 = sc.nextInt();
+//                if()
+//            }
+//        }
+
+        int app0 = 0, app1 = 0;
+        System.out.println("Welcome to FutureBuilder");
+        System.out.println("    1.) Enter the Application");
+        System.out.println("    2.) Exit the Application");
+        Scanner sc = new Scanner(System.in);
+
+        while(app0 != 2){ //application loop
+            app0 = sc.nextInt();
+            if(app0>2){  //ErrorGen: Warning if input is anything other than 1,2
+                System.out.println("Wrong choice entered. Exiting...");
+                break;
+            }
+            else{
+                System.out.println("Choose the mode you want to Enter in:- ");
+                System.out.println("    1.) Enter as Student Mode");
+                System.out.println("    2.) Enter as Company Mode");
+                System.out.println("    3.) Enter as Placement Cell Mode");
+                System.out.println("    4.) Return to main Application");
+                app1 = sc.nextInt();
+                switch(app1){
+                    ca
+                }
+
+            }
+        }
+    }
+
+    public void student(){
+
+    }
+
+    public void company(){
+
+    }
+
+    public void placementCell()
+    {
+        System.out.println("Welcome to IIITD Placement Cell :))");
+        System.out.println();
+    }
+}
