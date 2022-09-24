aa53e724d85bd9d6069833c018087f5cde6bf073
Tushar Chandra <64960756+me-tusharchandra@users.noreply.github.com>
2022-09-25 02:55:49 +0530

    update

--- Assignment_1/src/Main.java
+++ Assignment_1/src/Main.java
@@ -3,71 +3,65 @@
 
 import java.util.Scanner;
 
-
-
 public class Main {
+    static int app0, app1 = 0;
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-//        int app0 = 0, app1 = 0;
-//        System.out.println("Welcome to FutureBuilder");
-//        System.out.println("    1.) Enter the Application");
-//        System.out.println("    2.) Exit the Application");
-//        Scanner sc = new Scanner(System.in); //input scanner
-//
-//        while(app0 != 2){ //application loop
-//            app0 = sc.nextInt();
-//            if(app0>2){  //ErrorGen: Warning if input is anything other than 1,2
-//                System.out.println("Wrong choice entered. Exiting...");
-//                break;
-//            }
-//            else{
-//                System.out.println("Choose the mode you want to Enter in:- ");
-//                System.out.println("    1.) Enter as Student Mode");
-//                System.out.println("    2.) Enter as Company Mode");
-//                System.out.println("    3.) Enter as Placement Cell Mode");
-//                System.out.println("    4.) Return to main Application");
-//                app1 = sc.nextInt();
-//                if()
-//            }
-//        }
 
-        int app0 = 0, app1 = 0;
-        System.out.println("Welcome to FutureBuilder");
-        System.out.println("    1.) Enter the Application");
-        System.out.println("    2.) Exit the Application");
-        Scanner sc = new Scanner(System.in);
-
-        while(app0 != 2){ //application loop
+        while (app0 != 2) {
+            System.out.println("Welcome to FutureBuilder");
+            System.out.println("    1.) Enter the Application");
+            System.out.println("    2.) Exit the Application");
             app0 = sc.nextInt();
-            if(app0>2){  //ErrorGen: Warning if input is anything other than 1,2
-                System.out.println("Wrong choice entered. Exiting...");
-                break;
+            if (app0 > 2 || app0 < 1) {
+                System.out.println("Wrong choice entered. Try Again !!");
             }
-            else{
-                System.out.println("Choose the mode you want to Enter in:- ");
-                System.out.println("    1.) Enter as Student Mode");
-                System.out.println("    2.) Enter as Company Mode");
-                System.out.println("    3.) Enter as Placement Cell Mode");
-                System.out.println("    4.) Return to main Application");
-                app1 = sc.nextInt();
-                switch(app1){
-                    ca
-                }
-
+            else if(app0 == 1){
+                application();
             }
         }
     }
 
-    public void student(){
+    public static void application(){
+        System.out.println("Choose the mode you want to Enter in:-");
+        System.out.println("    1.) Enter as Student Mode");
+        System.out.println("    2.) Enter as Company Mode");
+        System.out.println("    3.) Enter as Placement Cell Mode");
+        System.out.println("    4.) Return to Main Application");
+        app1 = sc.nextInt();
+        if (app1 == 1) {
+            student(); // calling student
+        }
+        else if(app1 == 2){
+            company(); // calling company
+        }
+        else if(app1 == 3){
+            placementCell(); // calling placemenCell
+        }
+        else System.out.println("Returning to Main Application...");
+        app0 = 0;
+    }
+
+    public static void student(){ //Student Method
 
     }
 
-    public void company(){
+    public static void company(){ //Company Method
 
     }
 
-    public void placementCell()
-    {
-        System.out.println("Welcome to IIITD Placement Cell :))");
-        System.out.println();
+    public static void placementCell(){
+        System.out.println("Welcome to IIIT-D Placement Cell");
+        System.out.println("    1.) Open Student Registrations");
+        System.out.println("    2.) Open Company Registrations");
+        System.out.println("    3.) Get Number of Student Registrations");
+        System.out.println("    4.) Get Number of Company Registrations");
+        System.out.println("    5.) Get Number of Offered/Un-offered/Blocked Students");
+        System.out.println("    6.) Get Student Details");
+        System.out.println("    7.) Get Company Details");
+        System.out.println("    8.) Get Average Package");
+        System.out.println("    9.) Get Company Process Results");
+        System.out.println("    10.) Back");
     }
 }
--- Intro Assignment/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-    String name = "Tushar";
-        System.out.println(name.charAt(1));
-    }
-}
